fc26c137a513d02e239f12344247b746a7e5ca3a
CallmeTorre <alexis_torreblanca@outlook.com>
2017-08-23 21:11:36 -0500

    Practica 1 terminada

--- Practica 1/Euclides.py
+++ Practica 1/Euclides.py
@@ -1,16 +1,38 @@
 #!/usr/bin/python
 
 def euclides(m,n):
-    """Algoritmo de Euclides para encontrar el mcd de dos numeros enteros positivos m y n"""
+    """Funcion que recibe dos numeros enteros positivos m y n y retorna su MCD (Maximo Comun Multiplo)"""
+    cont = 0
     r = 0
-    while n!= 0:
+    cont +=1
+    cont +=1
+    while n != 0:
         r = m%n
+        cont +=1
         m = n
+        cont +=1
         n = r
-    return m
+        cont +=1
+    return cont#, m
+
+def fibonacci(n):
+    """Funcion que genera la secuencia de Fibonacci para un numero n."""
+    if n<0:
+        return None
+    elif n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        return fibonacci(n-1)+fibonacci(n-2)
 
 def main():
-    print(euclides(1000,120))
+    array = []
+    for i in range(1,25):
+        array.append(fibonacci(i))
+    for i in range(23,0,-1):
+        if(i>1):
+            print(euclides(array[i], array[i-1]),array[i],array[i-1])
 
 if __name__ == '__main__':
     main()
--- Practica 1/SumaBinaria.py
+++ Practica 1/SumaBinaria.py
@@ -1,39 +1,69 @@
 #!/usr/bin/python
+
 import random
 
 def suma(a,b):
+    """Funcion que recibe dos arreglos binarios A y B de longitud n y retorna la suma de ellos."""
+    cont = 0
     maxlen = max(len(a), len(b))
+    cont += 1
     result  = []
+    cont += 1
     carry   = 0
+    cont += 1
     i = maxlen - 1
+    cont += 1
 
+    cont += 1
     while(i >= 0):
         s = int(a[i]) + int(b[i])
+        cont += 1
+        cont += 1
         if s == 2: #1+1
+            cont += 1
             if carry == 0:
                 carry = 1
+                cont += 1
                 result.append(0)
+                cont += 1
             else:
+                cont += 1
                 result.append(1)
+                cont += 1
         elif s == 1: # 1+0
+            cont += 1
+            cont += 1
             if carry == 1:
                 result.append(0)
+                cont += 1
             else:
+                cont += 1
                 result.append(1)
+                cont += 1
         else: # 0+0
+            cont += 1
             if carry == 1:
+                cont += 1
                 result.append(1)
+                cont += 1
                 carry = 0
+                cont += 1
             else:
+                cont += 1
                 result.append(0)
+                cont += 1
         i = i - 1;
+        cont += 1
 
+    cont += 1
     if carry>0:
         result.append(1)
+        cont += 1
 
-    return result[::-1]
+    return cont #,result[::-1]
 
 def rellena(n):
+    """Funcion que genera dos numeros binarios aleatorios de longitud n y los almacena en dos arreglos"""
     a = []
     b = []
     for x in range(0,n):
@@ -42,16 +72,12 @@ def rellena(n):
     return a[::-1],b[::-1]
 
 def main():
-    for x in range(1,4):
+    for x in range(1,16):
+        c = 0
         n = pow(2,x)
         a,b =  rellena(n)
-        print("Arreglo 1: ")
-        print(a)
-        print("\n")
-        print("Arreglo 2: ")
-        print(b)
-        print("Suma: ")
-        print(suma(a,b))
+        c = suma(a,b)
+        print("Potencia: %d, Numero de operaciones:%d" %(n,c))
 
 if __name__ == '__main__':
     main()
