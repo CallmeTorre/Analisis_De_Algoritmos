09f40dc9bb1b883dab02c4d2ed5535d09b64c046
CallmeTorre <alexis_torreblanca@outlook.com>
2017-12-04 22:28:34 -0600

    Vertex cover with bugs in diagonals

--- Extra/VertexCover.py
+++ Extra/VertexCover.py
@@ -6,38 +6,54 @@ def vertexCover(graph):
     for i in range(size):
         if(visited[i] == None):
             lista = graph[i]
-            for j,elem in enumerate(lista):
-                if nextElem(j,len(lista)) == None:
-                    break
-                siguiente = lista[j+1]
-                if(visited[siguiente] == None):
-                    print("iteracion %d elem %d" %(i,elem))
-                    visited[i] = True
-                    visited[elem] = True
-                    if(i not in solution):
-                        solution.append(i)
+            for elem in lista:
+                print("iteracion %d elem %d" %(i,elem))
+                visited[i] = True
+                visited[elem] = True
+                if(i not in solution):
+                    solution.append(i)
     print(solution)
 
-def nextElem(index,lenArray):
-    if index+1 == lenArray:
-        return None
-    return True
-#a = {0:[1,2],
-# 1:[0],
-# 2:[0,3],
-# 3:[2]}
-#a = {0:[1,2],
-#  1:[0,3],
-#  2:[0],
-#  3:[1,4],
-#  4:[3,5],
-#  5:[4,6],
-#  6:[5]}
-#a = {0:[1],
-#    1:[0,2],
-#    2:[1,3,4],
-#    3:[2,4,5],
-#    4:[2,3,5,6],
-#    5:[3,4],
-#    6:[4]}
+a = {0:[1,2],
+ 1:[0],
+ 2:[0,3],
+ 3:[1,2]}
+b = {0:[1,2],
+  1:[0,3],
+  2:[0],
+  3:[1,4],
+  4:[3,5],
+  5:[4,6],
+  6:[5]}
+c = {0:[1],
+    1:[0,2],
+    2:[1,3,4],
+    3:[2,5],
+    4:[2,5,6],
+    5:[3,4],
+    6:[4]}
+print("Grafo:")
+print("(0)---------(1)")
+print(" |           |")
+print(" |           |")
+print(" |           |")
+print("(2)---------(3)")
 vertexCover(a)
+print("Grafo:")
+print("(0)---------(2)")
+print(" | ")
+print(" | ")
+print("(1)---------(3)")
+print("             |")
+print("             |")
+print("(5)---------(4)")
+print(" | ")
+print(" | ")
+print("(6)")
+vertexCover(b)
+print("(1)---------(2)---------(4)---------(6)")
+print(" |           |           |")
+print(" |           |           |")
+print(" |           |           |")
+print("(0)         (3)---------(5)")
+vertexCover(c)
